5ca9ce4afd7c35b4d957fc2baee532c226c244f7
Paul O'Connor <paul@poconnor.me>
2013-11-27 11:51:27 +0000

    Update Omnigraffle version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class omnigraffle {
   package { 'OmniGraffle':
     provider => 'appdmg_eula',
-    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.dmg'
+    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.2.dmg'
   }
 }
