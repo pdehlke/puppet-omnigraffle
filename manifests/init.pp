5ff68c7f44886883847956acbca299d8f807ab23
pdehlke <pde@rfc822.net>
2014-06-06 08:54:03 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class omnigraffle {
   package { 'OmniGraffle':
     provider => 'appdmg_eula',
-    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.4.dmg'
+    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.5.dmg'
   }
 }
